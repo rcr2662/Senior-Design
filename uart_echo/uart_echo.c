48490ee0d731032ea57edc6266e12fd2cc401e43
travispalmer6 <61255052+travispalmer6@users.noreply.github.com>
2023-01-30 14:40:07 -0600

    Add files via upload

---
+++ uart_echo/uart_echo.c
@@ -0,0 +1,261 @@
+//*****************************************************************************
+//
+// uart_echo.c - Example for reading data from and writing data to the UART in
+//               an interrupt driven fashion.
+//
+// Copyright (c) 2012-2017 Texas Instruments Incorporated.  All rights reserved.
+// Software License Agreement
+// 
+// Texas Instruments (TI) is supplying this software for use solely and
+// exclusively on TI's microcontroller products. The software is owned by
+// TI and/or its suppliers, and is protected under applicable copyright
+// laws. You may not combine this software with "viral" open-source
+// software in order to form a larger program.
+// 
+// THIS SOFTWARE IS PROVIDED "AS IS" AND WITH ALL FAULTS.
+// NO WARRANTIES, WHETHER EXPRESS, IMPLIED OR STATUTORY, INCLUDING, BUT
+// NOT LIMITED TO, IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE. TI SHALL NOT, UNDER ANY
+// CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL, OR CONSEQUENTIAL
+// DAMAGES, FOR ANY REASON WHATSOEVER.
+// 
+// This is part of revision 2.1.4.178 of the EK-TM4C123GXL Firmware Package.
+//
+//*****************************************************************************
+
+#include <stdint.h>
+#include <stdbool.h>
+#include "inc/hw_ints.h"
+#include "inc/hw_memmap.h"
+#include "driverlib/debug.h"
+#include "driverlib/fpu.h"
+#include "driverlib/gpio.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/pin_map.h"
+#include "driverlib/rom.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/uart.h"
+
+//*****************************************************************************
+//
+//! \addtogroup example_list
+//! <h1>UART Echo (uart_echo)</h1>
+//!
+//! This example application utilizes the UART to echo text.  The first UART
+//! (connected to the USB debug virtual serial port on the evaluation board)
+//! will be configured in 115,200 baud, 8-n-1 mode.  All characters received on
+//! the UART are transmitted back to the UART.
+//
+//*****************************************************************************
+
+//*****************************************************************************
+//
+// The error routine that is called if the driver library encounters an error.
+//
+//*****************************************************************************
+#ifdef DEBUG
+void
+__error__(char *pcFilename, uint32_t ui32Line)
+{
+}
+#endif
+
+//*****************************************************************************
+//
+// The UART interrupt handler.
+//
+//*****************************************************************************
+void
+UARTIntHandler(void)
+{
+    uint32_t ui32Status;
+
+    //
+    // Get the interrrupt status.
+    //
+    ui32Status = ROM_UARTIntStatus(UART0_BASE, true);
+
+    //
+    // Clear the asserted interrupts.
+    //
+    ROM_UARTIntClear(UART0_BASE, ui32Status);
+
+    //
+    // Loop while there are characters in the receive FIFO.
+    //
+    while(ROM_UARTCharsAvail(UART0_BASE))
+    {
+        //
+        // Read the next character from the UART and write it back to the UART.
+        //
+        ROM_UARTCharPutNonBlocking(UART0_BASE,
+                                   ROM_UARTCharGetNonBlocking(UART0_BASE));
+
+        //
+        // Blink the LED to show a character transfer is occuring.
+        //
+        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, GPIO_PIN_2);
+
+        //
+        // Delay for 1 millisecond.  Each SysCtlDelay is about 3 clocks.
+        //
+        SysCtlDelay(SysCtlClockGet() / (1000 * 3));
+
+        //
+        // Turn off the LED
+        //
+        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, 0);
+
+    }
+}
+
+//*****************************************************************************
+//
+// Send a string to the UART.
+//
+//*****************************************************************************
+void
+UARTSend(uint32_t ui32Base, const uint8_t *pui8Buffer, uint32_t ui32Count)
+{
+    int i;
+    for(i=0; i<ui32Count; i++){
+        UARTCharPut(ui32Base, pui8Buffer[i]);
+    }
+
+
+    //
+    // Loop while there are more characters to send.
+    //
+    /*while(ui32Count--)
+    {
+        //
+        // Write the next character to the UART.
+        //
+        //ROM_UARTCharPutNonBlocking(UART0_BASE, *pui8Buffer++);
+    }*/
+}
+
+//*****************************************************************************
+//
+// This example demonstrates how to send a string of data to the UART.
+//
+//*****************************************************************************
+int
+main(void)
+{
+    //
+    // Enable lazy stacking for interrupt handlers.  This allows floating-point
+    // instructions to be used within interrupt handlers, but at the expense of
+    // extra stack usage.
+    //
+    ROM_FPUEnable();
+    ROM_FPULazyStackingEnable();
+
+    //
+    // Set the clocking to run directly from the crystal.
+    //
+    ROM_SysCtlClockSet(SYSCTL_SYSDIV_1 | SYSCTL_USE_OSC | SYSCTL_OSC_MAIN |
+                       SYSCTL_XTAL_16MHZ);
+
+    //
+    // Enable the GPIO port that is used for the on-board LED.
+    //
+    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+
+    //
+    // Enable the GPIO pins for the LED (PF2).
+    //
+    ROM_GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_2);
+
+    //
+    // Enable the peripherals used by this example.
+    //
+    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
+    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+
+    //
+    // Enable processor interrupts.
+    //
+    ROM_IntMasterEnable();
+
+    //
+    // Set GPIO A0 and A1 as UART pins.
+    //
+    GPIOPinConfigure(GPIO_PA0_U0RX);
+    GPIOPinConfigure(GPIO_PA1_U0TX);
+    ROM_GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
+
+    //
+    // Configure the UART for 115,200, 8-N-1 operation.
+    //
+    ROM_UARTConfigSetExpClk(UART0_BASE, ROM_SysCtlClockGet(), 115200,
+                            (UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE |
+                             UART_CONFIG_PAR_NONE));
+
+    //
+    // Enable the UART interrupt.
+    //
+    ROM_IntEnable(INT_UART0);
+    ROM_UARTIntEnable(UART0_BASE, UART_INT_RX | UART_INT_RT);
+
+    int32_t tempChar = 0;
+    uint8_t rx_buffer[10];
+    memset(rx_buffer, 0, sizeof(*rx_buffer));
+
+
+    //
+    // Loop forever echoing data through the UART.
+    //
+    while(1)
+    {
+        // prompt user input
+        UARTSend(UART0_BASE, (uint8_t *)"Program or Readout? (P/R): ", 27);
+
+        // load user response into rx_buffer
+        int i=0;
+        do{
+            rx_buffer[i] = (uint8_t) UARTCharGet(UART0_BASE);
+            i++;
+        }while((char) rx_buffer[i-1] != '\n'); // might need to consider '\r' as a message terminator as well
+
+
+        // write rx_buffer to UART console
+        if(rx_buffer[0] == 'P'){
+            UARTSend(UART0_BASE, (uint8_t *)"Entering Programming Mode: \n", 28);
+        }else if (rx_buffer[0] == 'R'){
+            UARTSend(UART0_BASE, (uint8_t *)"Entering Readout Mode: \n", 24);
+        }else{
+            UARTSend(UART0_BASE, (uint8_t *)"Invalid Input\n", 14);
+        }
+
+        // clear buffer for next input
+        memset(rx_buffer, 0, sizeof(*rx_buffer));
+
+
+        // read line into rx_buffer
+//        int i=0;
+//        do{
+//            rx_buffer[i] = (uint8_t) UARTCharGet(UART0_BASE);
+//            i++;
+//        }while((char) rx_buffer[i-1] != '\n'); // might need to consider '\r' as a message terminator as well
+//
+//        // write rx_buffer to UART console
+//        int j;
+//        for(j=0; j<10; j++){
+//            UARTCharPut(UART0_BASE, '+');
+//            UARTCharPut(UART0_BASE, (unsigned char) rx_buffer[j]);
+//            rx_buffer[j]  = 0;
+//        }
+
+        /*
+        // reads one char and handles 'a', 'b', and 'default' cases
+        tempChar = UARTCharGet(UART0_BASE);
+        if(tempChar == 0x61){
+            UARTSend((uint8_t *)"\033[2JEnter aaaa: ", 16);
+        }else if (tempChar == 0x62){
+            UARTSend((uint8_t *)"\033[2JEnter bbbb: ", 16);
+        }
+        UARTSend((uint8_t *)"\033[2JEnter text: ", 16);
+        */
+    }
+}
