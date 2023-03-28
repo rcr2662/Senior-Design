36337fa989ebbd9fb871e115e65ccfcfddd18874
travispalmer6 <61255052+travispalmer6@users.noreply.github.com>
2023-03-27 23:17:17 -0500

    Add files via upload
    completely reformatted and reorganized code. similar to last version, near complete product but now more organized

--- test_equipment.c
+++ test_equipment.c
@@ -1,28 +1,3 @@
-//*****************************************************************************
-//
-// uart_echo.c - Example for reading data from and writing data to the UART in
-//               an interrupt driven fashion.
-//
-// Copyright (c) 2012-2017 Texas Instruments Incorporated.  All rights reserved.
-// Software License Agreement
-// 
-// Texas Instruments (TI) is supplying this software for use solely and
-// exclusively on TI's microcontroller products. The software is owned by
-// TI and/or its suppliers, and is protected under applicable copyright
-// laws. You may not combine this software with "viral" open-source
-// software in order to form a larger program.
-// 
-// THIS SOFTWARE IS PROVIDED "AS IS" AND WITH ALL FAULTS.
-// NO WARRANTIES, WHETHER EXPRESS, IMPLIED OR STATUTORY, INCLUDING, BUT
-// NOT LIMITED TO, IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-// A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE. TI SHALL NOT, UNDER ANY
-// CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL, OR CONSEQUENTIAL
-// DAMAGES, FOR ANY REASON WHATSOEVER.
-// 
-// This is part of revision 2.1.4.178 of the EK-TM4C123GXL Firmware Package.
-//
-//*****************************************************************************
-
 #include <stdint.h>
 #include <stdbool.h>
 #include "inc/hw_ints.h"
@@ -38,16 +13,15 @@
 #include "driverlib/comp.h"
 #include "driverlib/timer.h"
 
+#ifdef DEBUG
+void
+__error__(char *pcFilename, uint32_t ui32Line)
+{
+}
+#endif
+
 #define PACKET_LENGTH 19
 #define USER_INPUT_BUFFER_LENGTH 32
-
-// State machine variables
-// State Enumerations
-enum MODES {
-  PROGRAMMING_MODE,
-  READOUT_MODE,
-  UNKNOWN
-};
 
 enum states
 {
@@ -56,125 +30,120 @@ enum states
     Tx_mode,
     Rx_mode
 };
+
+// global state variables
+// needed to be global to be used by readout timer handler function
 enum states state = PC_initial;
 enum states next_state;
 
-//*****************************************************************************
-//
-//! \addtogroup example_list
-//! <h1>UART Echo (uart_echo)</h1>
-//!
-//! This example application utilizes the UART to echo text.  The first UART
-//! (connected to the USB debug virtual serial port on the evaluation board)
-//! will be configured in 115,200 baud, 8-n-1 mode.  All characters received on
-//! the UART are transmitted back to the UART.
-//
-//*****************************************************************************
 
-//*****************************************************************************
-//
-// The error routine that is called if the driver library encounters an error.
-//
-//*****************************************************************************
-#ifdef DEBUG
-void
-__error__(char *pcFilename, uint32_t ui32Line)
-{
-}
-#endif
 
-void comparator_timer(void);
+/////////////////////////////////////////////
+//          initialization functions
+/////////////////////////////////////////////
 
-//*****************************************************************************
-//
-// The UART interrupt handler.
-//
-//*****************************************************************************
-void
-UARTIntHandler(void)
-{
-    uint32_t ui32Status;
+// contains initialization code from the starter project
+void init_clock(void){
+    ROM_FPUEnable();
+    ROM_FPULazyStackingEnable();
 
-    //
-    // Get the interrrupt status.
-    //
-    ui32Status = ROM_UARTIntStatus(UART0_BASE, true);
-
-    //
-    // Clear the asserted interrupts.
-    //
-    ROM_UARTIntClear(UART0_BASE, ui32Status);
-
-    //
-    // Loop while there are characters in the receive FIFO.
-    //
-    while(ROM_UARTCharsAvail(UART0_BASE))
-    {
-        //
-        // Read the next character from the UART and write it back to the UART.
-        //
-        ROM_UARTCharPutNonBlocking(UART0_BASE,
-                                   ROM_UARTCharGetNonBlocking(UART0_BASE));
-
-        //
-        // Blink the LED to show a character transfer is occuring.
-        //
-        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, GPIO_PIN_2);
-
-        //
-        // Delay for 1 millisecond.  Each SysCtlDelay is about 3 clocks.
-        //
-        SysCtlDelay(SysCtlClockGet() / (1000 * 3));
-
-        //
-        // Turn off the LED
-        //
-        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, 0);
-
-    }
+    ROM_SysCtlClockSet(SYSCTL_SYSDIV_1 | SYSCTL_USE_OSC | SYSCTL_OSC_MAIN | SYSCTL_XTAL_16MHZ);
 }
 
-//*****************************************************************************
-//
-// Send a string to the UART.
-//
-//*****************************************************************************
-//void
-//UARTSend(const uint8_t *pui8Buffer, uint32_t ui32Count)
-//{
-//    //
-//    // Loop while there are more characters to send.
-//    //
-//    while(ui32Count--)
-//    {
-//        //
-//        // Write the next character to the UART.
-//        //
-//        ROM_UARTCharPutNonBlocking(UART0_BASE, *pui8Buffer++);
-//    }
-//}
+// initializes on-board LED GPIO outputs
+// PF1 = comparator digital output
+// PF3 = power enable output
+void init_leds(void){
+    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+    ROM_GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_3);
+}
 
+// initializes UART0 and UART1
+// UART0 Rx = PA0, Tx = PA1
+// UART1 Rx = PB0, Tx = PB1
+void init_uart(void){
+    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
+    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_UART1);
+    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);
 
-// intialize comparator
+    GPIOPinConfigure(GPIO_PA0_U0RX);
+    GPIOPinConfigure(GPIO_PA1_U0TX);
+    ROM_GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
+
+    GPIOPinConfigure(GPIO_PB0_U1RX);
+    GPIOPinConfigure(GPIO_PB1_U1TX);
+    ROM_GPIOPinTypeUART(GPIO_PORTB_BASE, GPIO_PIN_0 | GPIO_PIN_1);
+
+    // Configure both UARTs for 115,200, 8-N-1 operation.
+    ROM_UARTConfigSetExpClk(UART0_BASE, ROM_SysCtlClockGet(), 115200,
+                            (UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE |
+                             UART_CONFIG_PAR_NONE));
+
+    ROM_UARTConfigSetExpClk(UART1_BASE, ROM_SysCtlClockGet(), 9600,
+                                (UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE |
+                                 UART_CONFIG_PAR_NONE));
+}
+
 void init_comparator(){
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOC);
     SysCtlPeripheralEnable(SYSCTL_PERIPH_COMP0);
 
     GPIOPinTypeComparator(GPIO_PORTC_BASE, GPIO_PIN_7);
 
-    // Enable the COMP module.
     SysCtlPeripheralEnable(SYSCTL_PERIPH_COMP0);
-
-    // Wait for the COMP module to be ready.
     while(!SysCtlPeripheralReady(SYSCTL_PERIPH_COMP0)){}
 
     ComparatorRefSet(COMP_BASE, COMP_REF_0_275V);
     ComparatorConfigure(COMP_BASE, 0,(COMP_TRIG_NONE | COMP_ASRCP_REF | COMP_OUTPUT_INVERT));
-    SysCtlDelay(100000);
 
+    GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1, GPIO_PIN_1);
+
+    SysCtlDelay(100000);
 }
 
-void failed_readout() {
+// initialize TIMER0 and TIMER1
+// TIMER0 is for sampling the comparator and TIMER1 is the readout timer
+void init_timer(){
+    // Enable the Timer0 peripheral
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER0);
+    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_TIMER0)){}
+
+    TimerConfigure(TIMER0_BASE, TIMER_CFG_PERIODIC);
+    TimerLoadSet(TIMER0_BASE, TIMER_A, 416);
+    TimerIntRegister(TIMER0_BASE, TIMER_A, comparator_timer_handler);
+
+    TimerIntEnable(TIMER0_BASE, TIMER_TIMA_TIMEOUT);
+    TimerEnable(TIMER0_BASE, TIMER_A);
+
+    // Enable the Timer1 peripheral
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER1);
+    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_TIMER1)){}
+
+    TimerConfigure(TIMER1_BASE, TIMER_CFG_ONE_SHOT);
+    TimerLoadSet(TIMER1_BASE, TIMER_A, 16000000);
+    TimerIntRegister(TIMER1_BASE, TIMER_A, readout_timer_handler);
+
+    TimerIntEnable(TIMER1_BASE, TIMER_TIMA_TIMEOUT);
+    //TimerEnable(TIMER1_BASE, TIMER_A); // timer is enabled when MCU enters Readout Mode
+}
+
+/////////////////////////////////////////////
+//          Timer handler functions
+/////////////////////////////////////////////
+
+// function samples the output of the comparator and relays the signal to a digital pin (PF1)
+void comparator_timer_handler(void){
+    TimerIntClear(TIMER0_BASE, TIMER_TIMA_TIMEOUT);
+    if(ComparatorValueGet(COMP_BASE,0) == true){
+        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1, GPIO_PIN_1);
+    }else{
+        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1, 0);
+    }
+}
+
+// function is called when readout timer expires without receiving a message
+void readout_timer_handler() {
     TimerIntClear(TIMER1_BASE, TIMER_TIMA_TIMEOUT);
 
     UARTSendString(UART0_BASE, (uint8_t *) "No message received.\n", 21);
@@ -183,38 +152,22 @@ void failed_readout() {
     next_state = PC_initial;
 }
 
-// initialize the comparator timer
-void init_timer(){
-    // Enable the Timer0 peripheral
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER0);
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER1);
+/////////////////////////////////////////////
+//          UART functions
+/////////////////////////////////////////////
 
-    // Wait for the Timer0 module to be ready.
-    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_TIMER0)){}
+void UARTIntHandler(void){ // code from starter project mostly, don't think this function gets used
 
-    TimerConfigure(TIMER0_BASE, TIMER_CFG_PERIODIC);
-    TimerLoadSet(TIMER0_BASE, TIMER_A, 416);
-    TimerIntRegister(TIMER0_BASE, TIMER_A, comparator_timer);
+    uint32_t ui32Status;
+    ui32Status = ROM_UARTIntStatus(UART0_BASE, true);
+    ROM_UARTIntClear(UART0_BASE, ui32Status);
 
-    TimerIntEnable(TIMER0_BASE, TIMER_TIMA_TIMEOUT);
-    TimerEnable(TIMER0_BASE, TIMER_A);
-
-    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_TIMER1)){}
-
-    TimerConfigure(TIMER1_BASE, TIMER_CFG_ONE_SHOT);
-    TimerLoadSet(TIMER1_BASE, TIMER_A, 16000000);
-    TimerIntEnable(TIMER1_BASE, TIMER_TIMA_TIMEOUT);
-    TimerIntRegister(TIMER1_BASE, TIMER_A, failed_readout);
-    //TimerEnable(TIMER1_BASE, TIMER_A);
-
-}
-
-void comparator_timer(void){
-    TimerIntClear(TIMER0_BASE, TIMER_TIMA_TIMEOUT);
-    if(ComparatorValueGet(COMP_BASE,0) == true){
-        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1, GPIO_PIN_1);
-    }else{
-        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1, 0);
+    while(ROM_UARTCharsAvail(UART0_BASE))
+    {
+        ROM_UARTCharPutNonBlocking(UART0_BASE, ROM_UARTCharGetNonBlocking(UART0_BASE));
+        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, GPIO_PIN_2);
+        SysCtlDelay(SysCtlClockGet() / (1000 * 3));
+        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, 0);
     }
 }
 
@@ -226,7 +179,7 @@ void UARTSendString(uint32_t ui32Base, const uint8_t *pui8Buffer, uint32_t ui32C
     }
 }
 
-// Read string from the UART
+// Read string from the desired UART
 void UARTReadString(uint32_t ui32Base, uint8_t * rxBuffer){
     // load user response into rx_buffer
     int i=0;
@@ -235,6 +188,10 @@ void UARTReadString(uint32_t ui32Base, uint8_t * rxBuffer){
         i++;
     }while((char) rxBuffer[i-1] != '\n'); // might need to consider '\r' as a message terminator as well
 }
+
+/////////////////////////////////////////////
+//          Miscellaneous functions
+/////////////////////////////////////////////
 
 // this function checks if the user input is valid
 // the function checks if the user input is the correct length,
@@ -265,8 +222,6 @@ bool checkValidInput(uint8_t* buffer, uint8_t length){
     return true;
 }
 
-
-
 uint8_t rx_buffer[256] = {0};
 uint8_t tx_buffer[40] = {0};
 uint8_t user_input_buffer[USER_INPUT_BUFFER_LENGTH] = {0};
@@ -279,86 +234,11 @@ bool valid_data = false;
 int
 main(void)
 {
-    //
-    // Enable lazy stacking for interrupt handlers.  This allows floating-point
-    // instructions to be used within interrupt handlers, but at the expense of
-    // extra stack usage.
-    //
-    ROM_FPUEnable();
-    ROM_FPULazyStackingEnable();
-
-    //
-    // Set the clocking to run directly from the crystal.
-    //
-    ROM_SysCtlClockSet(SYSCTL_SYSDIV_1 | SYSCTL_USE_OSC | SYSCTL_OSC_MAIN |
-                       SYSCTL_XTAL_16MHZ);
-
-    //
-    // Enable the GPIO port that is used for the on-board LED.
-    //
-    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
-
-    // Enable the GPIO pins for the GPIO outputs and on-board LEDs
-    // PF1 = comparator output and PF3 = power control
-    ROM_GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_3);
-
-    //
-    // Enable the peripherals used by this example.
-    //
-    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
-    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
-    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_UART1);
-    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);
-
-    //
-    // Enable processor interrupts.
-    //
-    //ROM_IntMasterEnable();
-
-    //
-    // Set GPIO A0 and A1 as UART pins.
-    //
-    GPIOPinConfigure(GPIO_PA0_U0RX);
-    GPIOPinConfigure(GPIO_PA1_U0TX);
-    ROM_GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
-
-    GPIOPinConfigure(GPIO_PB0_U1RX);
-    GPIOPinConfigure(GPIO_PB1_U1TX);
-    ROM_GPIOPinTypeUART(GPIO_PORTB_BASE, GPIO_PIN_0 | GPIO_PIN_1);
-
-    //
-    // Configure the UART for 115,200, 8-N-1 operation.
-    //
-    ROM_UARTConfigSetExpClk(UART0_BASE, ROM_SysCtlClockGet(), 115200,
-                            (UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE |
-                             UART_CONFIG_PAR_NONE));
-
-    ROM_UARTConfigSetExpClk(UART1_BASE, ROM_SysCtlClockGet(), 9600,
-                                (UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE |
-                                 UART_CONFIG_PAR_NONE));
-
-    //
-    // Enable the UART interrupt.
-    //
-    //ROM_IntEnable(INT_UART0);
-    //ROM_UARTIntEnable(UART0_BASE, UART_INT_RX | UART_INT_RT);
-    //ROM_IntEnable(INT_UART1);
-    //ROM_UARTIntEnable(UART1_BASE, UART_INT_RX | UART_INT_RT);
-
-    // initialize comparator, comparator interrupt timer, and digital output pin
+    init_clock();
+    init_leds();
+    init_uart();
     init_comparator();
-    //GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_3);
-    GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1, GPIO_PIN_1);
     init_timer();
-
-    //
-    // Prompt for text to be entered.
-    //
-    //UARTSend((uint8_t *)"\033[2JEnter text: ", 16);
-
-    // Communication variables and flags
-    enum MODES OPERATING_MODE;
-    OPERATING_MODE = UNKNOWN;
 
     while(1){
 
@@ -381,11 +261,11 @@ main(void)
                    // prompt user for G1
 
 //                    read G1
-                   //do{
+//                   do{
                        memset(user_input_buffer, 0, sizeof(*user_input_buffer));
                        UARTSendString(UART0_BASE, (uint8_t *)"\nEnter Group Delay G1: ", 23);
                        UARTReadString(UART0_BASE, &user_input_buffer);
-                   //}while(checkValidInput(user_input_buffer, 4) == false);
+//                   while(checkValidInput(user_input_buffer, 4) == false);
 //                   if(checkValidInput(user_input_buffer, 4)){
 //                       UARTSendString(UART0_BASE, (uint8_t *)"valid\n", 6);
 //                   }else{
@@ -500,9 +380,6 @@ main(void)
 
               next_state = PC_initial;
 
-              // used for testing end-to-end data transmission
-              //next_state = Rx_mode;
-
           break;
           }
           case Rx_mode:
@@ -565,10 +442,6 @@ main(void)
                       rx_buffer[i] = '\0';
               }
 
-              // used for testing the hex to ascii conversion
-              //UARTSendString(UART0_BASE, (uint8_t *) rx_buffer, 38);
-              //UARTSendString(UART0_BASE, (uint8_t *) "\n", 1);
-
               UARTSendString(UART0_BASE, (uint8_t *)"Data Readout:\n", 14);
               UARTSendString(UART0_BASE, (uint8_t *)"G1: ", 4);
               UARTSendString(UART0_BASE, (uint8_t *)&rx_buffer[0], 4);
@@ -596,32 +469,4 @@ main(void)
         state = next_state;
     }
 
-
-
-
-    //
-    // Loop forever echoing data through the UART.
-    //
-//    while(1)
-//    {
-//        while(UARTCharsAvail(UART1_BASE) == false) {}
-//        uint8_t c = UARTCharGetNonBlocking(UART1_BASE);
-//        if(c != '\n' && c != '\r'){
-//            rx_buffer[rx_index] = c;
-//            rx_index++;
-//        }else{
-//            rx_buffer[rx_index] = '\0';
-//            rx_index = 0;
-//            GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, GPIO_PIN_2);
-//
-//            // output data to PC
-//            int i = 0;
-//            for(i=0; i<30; i++){
-//                UARTCharPut(UART0_BASE, rx_buffer[i]);
-//            }
-//        }
-//
-
-//
-//    while(1){}
 }
